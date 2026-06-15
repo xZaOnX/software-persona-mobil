4c0c77fa5b3d98708505ed3ade2846f75552d027
xZaOnX <ozanoney@gmail.com>
2026-06-15 19:04:09 +0300

    refactor: rename Product.imageUrl to imageAsset

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -6,13 +6,13 @@ class Product {
   final String name;
   final double price;
   final String description;
-  final String imageUrl;
+  final String imageAsset;
 
   const Product({
     required this.id,
     required this.name,
     required this.price,
     required this.description,
-    required this.imageUrl,
+    required this.imageAsset,
   });
 }
